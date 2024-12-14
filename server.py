b954317964e24694e0c66af7e2efbf0444964aed
gregsadetsky <lepetitg@gmail.com>
2024-12-14 15:06:11 -0500

    .

--- server.py
+++ server.py
@@ -1,14 +1,17 @@
 from datetime import datetime
 
+import whisper
 from flask import Flask
 
 app = Flask(__name__)
 
+model = whisper.load_model("turbo")
+
 
 @app.route("/")
 def hello_world():
-    print("new web request")
-    return f"hello from disco!!! the datetime is {datetime.now()}"
+    result = model.transcribe("whisper-example-audio.mp3")
+    return result["text"]
 
 
 if __name__ == "__main__":
